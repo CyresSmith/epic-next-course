8131fa10ffbb0be5965adbc9c75f3e291ea0b877
CyresSmith <madnezzz5@gmail.com>
2025-07-11 17:38:10 +0300

    Implement owner-based access control middleware and enhance summary actions

--- frontend/components/forms/summary-card-form.tsx
+++ frontend/components/forms/summary-card-form.tsx
@@ -1,17 +1,27 @@
-/// import { updateSummaryAction, deleteSummaryAction } from "@/data/actions/summary-actions";
+'use client';
+import { useActionState } from 'react';
+
+import { deleteSummaryAction, updateSummaryAction } from '@/data/actions/summary-actions';
 import { cn } from '@/lib/utils';
 
 import { Input } from '@/components/ui/input';
 import { Textarea } from '@/components/ui/textarea';
 
+import { StrapiErrors } from '@/components/custom/strapi-errors';
+
 import { Card, CardContent, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
 
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 
+import { DeleteButton } from '@/components/custom/delete-button';
 import { SubmitButton } from '@/components/custom/submit-button';
-// import { DeleteButton } from "@/components/custom/delete-button";
-
 import ReactMarkdown from 'react-markdown';
+
+const INITIAL_STATE = {
+    strapiErrors: null,
+    data: null,
+    message: null,
+};
 
 export function SummaryCardForm({
     item,
@@ -20,8 +30,11 @@ export function SummaryCardForm({
     readonly item: any;
     readonly className?: string;
 }) {
-    // const deleteSummaryById = deleteSummaryAction.bind(null, item.documentId);
+    const deleteSummaryById = deleteSummaryAction.bind(null, item.documentId);
 
+    const [deleteState, deleteAction] = useActionState(deleteSummaryById, INITIAL_STATE);
+
+    const [updateState, updateAction] = useActionState(updateSummaryAction, INITIAL_STATE);
     return (
         <Card className={cn('mb-8 relative h-auto', className)}>
             <CardHeader>
@@ -29,7 +42,7 @@ export function SummaryCardForm({
             </CardHeader>
             <CardContent>
                 <div>
-                    <form>
+                    <form action={updateAction}>
                         <Input
                             id="title"
                             name="title"
@@ -46,48 +59,28 @@ export function SummaryCardForm({
                                 </TabsList>
                                 <TabsContent
                                     value="preview"
-                                    className="flex-1 
-                      markdown-preview
-                      relative w-full h-[600px]
-                      overflow-auto scroll-smooth
-                      p-4 px-3 py-2
-                      text-sm
-                      bg-white dark:bg-gray-800 
-                      border border-gray-300 dark:border-gray-700
-                      rounded-md
-                      shadow-sm
-                      mb-4
-                      placeholder:text-muted-foreground
-                      focus-visible:outline-none
-                      focus-visible:bg-gray-50
-                      focus-visible:ring-1
-                      focus-visible:ring-ring
-                      disabled:cursor-not-allowed
-                      disabled:opacity-50"
+                                    className="
+                                    flex-1
+                    markdown-preview
+                    relative w-full h-[600px]
+                    overflow-auto scroll-smooth
+                    p-4 px-3 py-2
+                    text-sm
+                     dark:bg-gray-800 bg-transparent
+                    border border-gray-300 dark:border-gray-700
+                    rounded-md
+                    shadow-sm
+                    mb-4
+                    placeholder:text-muted-foreground
+                    focus-visible:outline-none
+                    focus-visible:bg-gray-50
+                    focus-visible:ring-1
+                    focus-visible:ring-ring
+                    disabled:cursor-not-allowed
+                    disabled:opacity-50
+                  "
                                 >
-                                    <ReactMarkdown
-                                    //     className="
-                                    //     markdown-preview
-                                    //     relative w-full h-[600px]
-                                    //     overflow-auto scroll-smooth
-                                    //     p-4 px-3 py-2
-                                    //     text-sm
-                                    //     bg-white dark:bg-gray-800
-                                    //     border border-gray-300 dark:border-gray-700
-                                    //     rounded-md
-                                    //     shadow-sm
-                                    //     mb-4
-                                    //     placeholder:text-muted-foreground
-                                    //     focus-visible:outline-none
-                                    //     focus-visible:bg-gray-50
-                                    //     focus-visible:ring-1
-                                    //     focus-visible:ring-ring
-                                    //     disabled:cursor-not-allowed
-                                    //     disabled:opacity-50
-                                    //   "
-                                    >
-                                        {item.summary}
-                                    </ReactMarkdown>
+                                    <ReactMarkdown>{item.summary}</ReactMarkdown>
                                 </TabsContent>
                                 <TabsContent value="markdown" className="flex-1">
                                     <Textarea
@@ -98,7 +91,7 @@ export function SummaryCardForm({
                       overflow-auto scroll-smooth
                       p-4 px-3 py-2
                       text-sm
-                      bg-white dark:bg-gray-800 
+                       dark:bg-gray-800 bg-transparent
                       border border-gray-300 dark:border-gray-700
                       rounded-md
                       shadow-sm
@@ -119,12 +112,14 @@ export function SummaryCardForm({
                         <input type="hidden" name="id" value={item.documentId} />
                         <SubmitButton text="Update Summary" loadingText="Updating Summary" />
                     </form>
-                    <form>
-                        {/* <DeleteButton className="absolute right-4 top-4 bg-red-700 hover:bg-red-600" /> */}
+                    <form action={deleteAction}>
+                        <DeleteButton className="absolute right-4 top-4 bg-red-700 hover:bg-red-600" />
                     </form>
                 </div>
             </CardContent>
-            <CardFooter></CardFooter>
+            <CardFooter>
+                <StrapiErrors error={deleteState?.strapiErrors || updateState?.strapiErrors} />
+            </CardFooter>
         </Card>
     );
 }
